7dcdb9042a95834b936cb15f9db2ba9be024eff8
Joshua Taylor <josh-taylor@uiowa.edu>
2021-08-30 20:45:08 -0500

    inital commit

---
+++ HealthApp/HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by Joshua Taylor on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
